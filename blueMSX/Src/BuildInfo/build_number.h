ed724f6f5b4095280edd075586aeaa5e4c44b3e8
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2004-12-13 16:02:46 +0000

    Fixed PCM sample timer
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@39 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 14818
+#define BUILD_NUMBER 14890
 
-#define BUILD_DATE "12/12 2004 5:40pm"
+#define BUILD_DATE "12/13 2004 12:7am"
 
 #endif
--- blueMSX/Src/Memory/romMapperTurboRPcm.c
+++ blueMSX/Src/Memory/romMapperTurboRPcm.c
@@ -1,9 +1,9 @@
 /*****************************************************************************
 ** $Source: /cygdrive/d/Private/_SVNROOT/bluemsx/blueMSX/Src/Memory/romMapperTurboRPcm.c,v $
 **
-** $Revision: 1.4 $
+** $Revision: 1.5 $
 **
-** $Date: 2004-12-12 22:18:27 $
+** $Date: 2004-12-13 16:02:46 $
 **
 ** More info: http://www.bluemsx.com
 **
@@ -42,16 +42,20 @@ typedef struct {
     int    deviceHandle;
     UInt8  sample;
     UInt8  status;
-    UInt32 time;
+    UInt8  time;
+    UInt32 refTime;
+    UInt32 refFrag;
 } RomMapperTurboRPcm;
 
 static void saveState(RomMapperTurboRPcm* rm)
 {
     SaveState* state = saveStateOpenForWrite("mapperTurboRPcm");
 
-    saveStateSet(state, "sample", rm->sample);
-    saveStateSet(state, "status", rm->status);
-    saveStateSet(state, "time",   rm->time);
+    saveStateSet(state, "sample",  rm->sample);
+    saveStateSet(state, "status",  rm->status);
+    saveStateSet(state, "time",    rm->time);
+    saveStateSet(state, "refTime", rm->refTime);
+    saveStateSet(state, "refFrag", rm->refFrag);
     
     saveStateClose(state);
 }
@@ -60,9 +64,11 @@ static void loadState(RomMapperTurboRPcm* rm)
 {
     SaveState* state = saveStateOpenForRead("mapperTurboRPcm");
 
-    rm->sample = (UInt8)saveStateGet(state, "sample", 0);
-    rm->status = (UInt8)saveStateGet(state, "status", 0);
-    rm->time   =        saveStateGet(state, "time",   boardSystemTime());
+    rm->sample  = (UInt8)saveStateGet(state, "sample",  0);
+    rm->status  = (UInt8)saveStateGet(state, "status",  0);
+    rm->time    = (UInt8)saveStateGet(state, "time",    0);
+    rm->refTime =        saveStateGet(state, "refTime", 0);
+    rm->refFrag =        saveStateGet(state, "refFrag", 0);
 
     saveStateClose(state);
 }
@@ -77,11 +83,24 @@ static void destroy(RomMapperTurboRPcm* rm)
     free(rm);
 }
 
+static UInt8 getTimerCounter(RomMapperTurboRPcm* rm)
+{
+    UInt64 elapsed;
+    UInt32 systemTime = boardSystemTime();
+
+    elapsed      = 15750 * (UInt64)(systemTime - rm->refTime) + rm->refFrag;
+    rm->refTime  = systemTime;
+    rm->refFrag  = (UInt32)(elapsed % boardFrequency());
+    rm->time += (UInt8)(elapsed / boardFrequency());
+
+    return rm->time & 0x03;
+}
+
 static UInt8 read(RomMapperTurboRPcm* rm, UInt16 ioPort)
 {
     switch (ioPort & 0x01) {
 	case 0: 
-		return (UInt8)(21000 * (UInt64)(boardSystemTime() - rm->time) / boardFrequency()) & 0x03;
+		return getTimerCounter(rm);
 	case 1:
 		return (~rm->sample & 0x80) | rm->status;
     }
@@ -92,7 +111,8 @@ static void write(RomMapperTurboRPcm* rm, UInt16 ioPort, UInt8 value)
 {
 	switch (ioPort & 0x01) {
 	case 0:
-		rm->time   = boardSystemTime();// - boardFrequency() / 15750 / 4;
+        getTimerCounter(rm);
+        rm->time = 0;
 		rm->sample = value;
 		if (rm->status & 0x02) {
             dacWrite(rm->dac, rm->sample);
@@ -108,16 +128,22 @@ static void write(RomMapperTurboRPcm* rm, UInt16 ioPort, UInt8 value)
     }
 }
 
+static void reset(RomMapperTurboRPcm* rm)
+{
+    rm->refTime = boardSystemTime();
+    rm->refFrag = 0;
+    rm->time    = 0;
+}
 int romMapperTurboRPcmCreate(DAC* dac) 
 {
-    DeviceCallbacks callbacks = { destroy, NULL, saveState, loadState };
+    DeviceCallbacks callbacks = { destroy, reset, saveState, loadState };
     RomMapperTurboRPcm* rm = malloc(sizeof(RomMapperTurboRPcm));
 
     rm->deviceHandle = deviceManagerRegister(ROM_TURBORPCM, &callbacks, rm);
 
     rm->dac    = dac;
 	rm->status = 0;
-    rm->time   = boardSystemTime();
+    rm->time   = 0;
 
     ioPortRegister(0xa4, read, write, rm);
     ioPortRegister(0xa5, read, write, rm);
