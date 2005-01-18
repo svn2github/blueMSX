d7570fe30ba3a6fa413c86b4f2f4e74d279cf09d
spectravision <spectravision@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2005-01-18 23:48:26 +0000

    no message
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@129 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/VideoChips/CRTC6845.c
+++ blueMSX/Src/VideoChips/CRTC6845.c
@@ -1,9 +1,9 @@
 /*****************************************************************************
 ** $Source: /cygdrive/d/Private/_SVNROOT/bluemsx/blueMSX/Src/VideoChips/CRTC6845.c,v $
 **
-** $Revision: 1.8 $
+** $Revision: 1.9 $
 **
-** $Date: 2005-01-18 19:04:22 $
+** $Date: 2005-01-18 23:48:26 $
 **
 ** More info: http://www.bluemsx.com
 **
@@ -35,7 +35,6 @@
 
 /*
      AR Address Register
-
      R0 Horizontal Total (Character)
      R1 Horizontal Displayed (Character)
      R2 Horizontal Sync Position (Character)
@@ -56,45 +55,40 @@
      R17 Light Pen (L)
 */
 
-static const UInt8 crtcRegisterValueMask[18] = {
-    0xff,  // R0
-    0xff,  // R1
-    0xff,  // R2
-    0xff,  // R3
-    0x7f,  // R4
-    0x1f,  // R5
-    0x7f,  // R6
-    0x7f,  // R7
-    0xf3,  // R8
-    0x1f,  // R9
-    0x7f,  // R10
-    0x1f,  // R11
-    0x3f,  // R12
-    0xff,  // R13
-    0x3f,  // R14
-    0xff,  // R15
-    0x3f,  // R16
-    0xff   // R17
-};
-
-static UInt8 crtcRegister[18];
-static UInt8 crtcAddressReg; // AR
-
 typedef enum { CURSOR_DISABLED, CURSOR_BLINK, CURSOR_NOBLINK} crtcCursorModes;
 
-/* Encoded registers */
+typedef enum { CRTC_SCREEN_NOREFRESH, CRTC_SCREEN_REFRESH } crtcScreenRefreshs;
+
+typedef enum { CRTC_R0=0,   CRTC_R1=1,   CRTC_R2=2,   CRTC_R3=3,   CRTC_R4=4,   CRTC_R5=5,
+               CRTC_R6=6,   CRTC_R7=7,   CRTC_R8=8,   CRTC_R9=9,   CRTC_R10=10, CRTC_R11=11,
+               CRTC_R12=12, CRTC_R13=13, CRTC_R14=14, CRTC_R15=15, CRTC_R16=16, CRTC_R17=17 } crtcRegisters;
+
+static const UInt8 crtcRegisterValueMask[18] = {
+    0xff, 0xff, 0xff, 0xff, 0x7f, 0x1f,
+    0x7f, 0x7f, 0xf3, 0x1f, 0x7f, 0x1f,
+    0x3f, 0xff, 0x3f, 0xff, 0x3f, 0xff 
+};
+
 typedef struct
 {
-    UInt8    mode;
+    int      mode;
     UInt8    rasterStart;
     UInt8    rasterEnd;
     UInt16   addressStart;
-    float   blinkrate;
-} TYP_CRTC_CURSOR;
+    float    blinkrate;
+} TYP_CURSOR;
 
 typedef struct
 {
-    TYP_CRTC_CURSOR    cursor;
+    UInt8    address;  // AR
+    UInt8    reg[18];  // R0-R17
+} TYP_REGISTER;
+
+typedef struct
+{
+    TYP_CURSOR   cursor;
+    TYP_REGISTER registers;
+    int          screen;
 } TYP_CRTC;
 
 static TYP_CRTC crtc;
@@ -103,43 +97,60 @@ static UInt8 crtcROM[0x1000];
 static UInt8 crtcMemory[0x800];
 static UInt8 crtcMemoryBankControl = 0;
 
+static int crtcConnector;
+
 // 80 x 25 characters (8x8 pixels/char, no colors)
 static UInt8 crtcScreenBuffer[0x3e80];
 
-static int crtcConnector;
-
-void crtcScreenRefresh(void)
-{
 /*
     - Blit crtcScreenBuffer to display
     - Reorganize buffer for VideoRender interface?
     - Draw cursor
     - Timer for blinking cursor
 */
-    int x, y, Nhd, Nvd;
-    UInt16 address;
 
-    address = 0;
+void crtcScreenDrawCursor(void)
+{
+    UInt8 cursorBmp[8];
+    int i;
+
+    memset(&cursorBmp[8], 0, sizeof(cursorBmp));
+
+    for (i = crtc.cursor.rasterStart; i < crtc.cursor.rasterEnd; i++) {
+        cursorBmp[i] = 0xff;
+    }
+
+    memcpy(&crtcScreenBuffer[crtc.cursor.addressStart], &cursorBmp[0], sizeof(cursorBmp));
+}
+
+void crtcScreenRefresh(void)
+{
+    int x, y, Nhd, Nvd;
+    UInt16 addressVram, addressScreen;
+
+    addressVram = 0;
+    addressScreen = 0;
     memset(crtcScreenBuffer, 0, sizeof(crtcScreenBuffer));
 
-    Nhd = crtcRegister[1];
-    if (Nhd >= crtcRegister[0])
-        Nhd = crtcRegister[0] - 1;
+    Nhd = crtc.registers.reg[CRTC_R1];
+    if (Nhd >= crtc.registers.reg[CRTC_R0])
+        Nhd = crtc.registers.reg[CRTC_R0] - 1;
     if (Nhd > 80)
         Nhd = 80;
 
-    Nvd = crtcRegister[6];
-    if (Nvd >= crtcRegister[4])
-        Nvd = crtcRegister[4] - 1;
+    Nvd = crtc.registers.reg[CRTC_R6];
+    if (Nvd >= crtc.registers.reg[CRTC_R4])
+        Nvd = crtc.registers.reg[CRTC_R4] - 1;
     if (Nvd > 25)
         Nvd = 25;
 
     for (y = 0; y < Nvd; y++) {
 
         for (x = 0; x < Nhd; x++) {
-            memcpy(&crtcScreenBuffer[address*8], &crtcROM[crtcMemory[address]], 8);
-            address++;
+            memcpy(&crtcScreenBuffer[addressScreen+x*8], &crtcROM[crtcMemory[addressVram]], 8);
+            addressVram++;
         }
+        addressScreen += 640;
     }
 }
 
@@ -151,7 +162,7 @@ static void crtcScreenWrite(UInt16 address, UInt8 value)
 
 static void crtcCursorUpdate(void)
 {
-    switch (crtcRegister[10] & 0x60 ) {
+    switch (crtc.registers.reg[CRTC_R10] & 0x60 ) {
     case 32:
         crtc.cursor.mode = CURSOR_DISABLED;
         crtc.cursor.blinkrate = 0;
@@ -165,36 +176,44 @@ static void crtcCursorUpdate(void)
         crtc.cursor.blinkrate = (float)50 / 32;    // Get Hz from emu
         break;
     default:
-        crtc.cursor.mode =CURSOR_NOBLINK;
+        crtc.cursor.mode = CURSOR_NOBLINK;
         crtc.cursor.blinkrate = 0;
     }
     
-    crtc.cursor.rasterStart = crtcRegister[10] & 0x1f;
-    crtc.cursor.rasterEnd = crtcRegister[11];
+    crtc.cursor.rasterStart = crtc.registers.reg[CRTC_R10] & 0x1f;
+    crtc.cursor.rasterEnd = crtc.registers.reg[CRTC_R11];
 
-    crtc.cursor.addressStart = crtcRegister[14];
+    crtc.cursor.addressStart = crtc.registers.reg[CRTC_R14];
     crtc.cursor.addressStart <<= 8;
-    crtc.cursor.addressStart |= crtcRegister[15];
+    crtc.cursor.addressStart |= crtc.registers.reg[CRTC_R15];
 }
 
 UInt8 crtcRead(void* dummy, UInt16 ioPort)
 {
-    if (crtcAddressReg < 18)
-        return crtcRegister[crtcAddressReg];
+    if (crtc.registers.address < 18)
+        return crtc.registers.reg[crtc.registers.address];
     else
         return 0xff;
 }
 
 void crtcWrite(void* dummy, UInt16 ioPort, UInt8 value)
 {
-    if (crtcAddressReg < 18) {
-        value &= crtcRegisterValueMask[crtcAddressReg];
-        crtcRegister[crtcAddressReg] = value;
-        switch (crtcAddressReg ) {
-        case 10:
-        case 11:
-        case 14:
-        case 15:
+    if (crtc.registers.address < 18) {
+
+        value &= crtcRegisterValueMask[crtc.registers.address];
+        crtc.registers.reg[crtc.registers.address] = value;
+
+        switch (crtc.registers.address) {
+        case CRTC_R0:
+        case CRTC_R1:
+        case CRTC_R4:
+        case CRTC_R6:
+            crtc.screen = CRTC_SCREEN_REFRESH;
+            break;
+        case CRTC_R10:
+        case CRTC_R11:
+        case CRTC_R14:
+        case CRTC_R15:
             crtcCursorUpdate();
             break;
         }
@@ -203,7 +222,7 @@ void crtcWrite(void* dummy, UInt16 ioPort, UInt8 value)
 
 void crtcWriteLatch(void* dummy, UInt16 ioPort, UInt8 value)
 {
-    crtcAddressReg = value & 0x1f;
+    crtc.registers.address = value & 0x1f;
 }
 
 void crtcMemEnable(void* dummy, UInt16 ioPort, UInt8 value)
@@ -234,9 +253,8 @@ UInt8 crtcMemRead(UInt16 address)
 
 void crtcReset(void)
 {
-    crtcAddressReg = 0;
     crtcMemoryBankControl = 0;
-    memset(crtcRegister, 0, sizeof(crtcRegister));
+    memset(&crtc, 0, sizeof(crtc));
     memset(crtcMemory, 0xff, sizeof(crtcMemory));
     memset(crtcScreenBuffer, 0, sizeof(crtcScreenBuffer));
 }
