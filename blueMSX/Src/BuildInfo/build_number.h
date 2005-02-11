7a96f5ff6dc7d8d6184f6a0ee4fdecc0af0ee1e1
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2005-02-11 05:14:10 +0000

    Added empty Debugger files
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@223 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 18663
+#define BUILD_NUMBER 18664
 
-#define BUILD_DATE "2/10 2005 8:35pm"
+#define BUILD_DATE "2/10 2005 8:39pm"
 
 #endif
