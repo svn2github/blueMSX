7c70eea1e9ce304c3edac7d95fdb081ff601d59d
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2005-07-02 17:59:21 +0000

    Added MidiIo
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@403 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

---
+++ blueMSX/Src/IoDevice/MidiIO.c
@@ -0,0 +1,122 @@
+/*****************************************************************************
+** $Source: /cygdrive/d/Private/_SVNROOT/bluemsx/blueMSX/Src/IoDevice/MidiIO.c,v $
+**
+** $Revision: 1.1 $
+**
+** $Date: 2005-07-02 17:59:21 $
+**
+** More info: http://www.bluemsx.com
+**
+** Copyright (C) 2003-2004 Daniel Vik, Tomas Karlsson
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+******************************************************************************
+*/
+#include <stdlib.h>
+#include "MidiIO.h"
+#include "ArchUart.h"
+#include "DAC.h"
+#include "Board.h"
+
+int archMidiCreate(void);
+void archMidiDestroy(void);
+int archMidiTransmit(UInt8 value);
+
+typedef struct MidiIO {
+    MidiType type;
+    FILE* file;
+    int  uartReady;
+};
+
+static MidiType theMidiOutType = MIDI_NONE;
+static MidiIO* theMidiIO = NULL;
+static char theFileName[512] = { 0 };
+
+
+static void setType(MidiIO* midiIo)
+{
+    midiIo->type = theMidiOutType;
+
+    switch (midiIo->type) {
+    case MIDI_HOST:
+        midiIo->uartReady = archMidiCreate();
+        break;
+    case MIDI_FILE:
+        midiIo->file = fopen(theFileName, "w+");
+        break;
+    }
+}
+
+static void removeType(MidiIO* midiIo)
+{
+    switch (midiIo->type) {
+    case MIDI_HOST:
+        archMidiDestroy();
+        midiIo->uartReady = 0;
+        break;
+    case MIDI_FILE:
+        fclose(midiIo->file);
+        break;
+    }
+}
+
+void midiIoTransmit(MidiIO* midiIo, UInt8 value)
+{
+    switch (midiIo->type) {
+    case MIDI_HOST:
+        archMidiTransmit(value);
+        break;
+    case MIDI_FILE:
+        fwrite(&value, 1, 1, midiIo->file);
+        break;
+    }
+}
+
+MidiIO* midiIoCreate()
+{
+    MidiIO* midiIo = calloc(1, sizeof(MidiIO));
+
+    setType(midiIo);
+
+    theMidiIO = midiIo;
+
+    return midiIo;
+}
+
+void midiIoDestroy(MidiIO* midiIo)
+{
+    removeType(midiIo);
+
+    free(midiIo);
+
+    theMidiIO = NULL;
+}
+
+void midiIoSetMidiOutType(MidiType type, const char* fileName)
+{   
+    theMidiOutType = type;
+
+    strcpy(theFileName, fileName);
+    
+    if (theMidiIO == NULL) {
+        return;
+    }
+
+    removeType(theMidiIO);
+    setType(theMidiIO);
+}
---
+++ blueMSX/Src/IoDevice/MidiIO.h
@@ -0,0 +1,46 @@
+/*****************************************************************************
+** $Source: /cygdrive/d/Private/_SVNROOT/bluemsx/blueMSX/Src/IoDevice/MidiIO.h,v $
+**
+** $Revision: 1.1 $
+**
+** $Date: 2005-07-02 17:59:21 $
+**
+** More info: http://www.bluemsx.com
+**
+** Copyright (C) 2003-2004 Daniel Vikl, Tomas Karlsson
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+******************************************************************************
+*/
+#ifndef MIDI_IO_H
+#define MIDI_IO_H
+
+#include "MsxTypes.h"
+
+typedef struct MidiIO MidiIO;
+
+typedef enum { MIDI_NONE, MIDI_FILE, MIDI_HOST } MidiType;
+
+MidiIO* midiIoCreate();
+void midiIoDestroy(MidiIO* midiIo);
+
+void midiIoTransmit(MidiIO* midiIo, UInt8 value);
+
+void midiIoSetMidiOutType(MidiType type, const char* fileName);
+
+#endif
