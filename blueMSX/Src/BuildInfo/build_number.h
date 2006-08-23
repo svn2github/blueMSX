857bd37bf1b1c0dcc0a281e03c6e19f910d9971d
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2006-08-23 15:04:38 +0000

    Fixed bluemsxlite makefile
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@758 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 27792
+#define BUILD_NUMBER 27833
 
-#define BUILD_DATE "8/21 2006 5:15pm"
+#define BUILD_DATE "8/22 2006 11:54pm"
 
 #endif
