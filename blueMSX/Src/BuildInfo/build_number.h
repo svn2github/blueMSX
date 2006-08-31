1591463520dc668eb31abe936666e6198c2d6437
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2006-08-31 23:48:10 +0000

    Made packet dll loading dynamic
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@778 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 28354
+#define BUILD_NUMBER 28355
 
-#define BUILD_DATE "8/31 2006 3:3pm"
+#define BUILD_DATE "8/31 2006 3:34pm"
 
 #endif
