735a59f9997f9c154786787796c420446334981b
m9710797 <m9710797@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2007-05-23 09:29:30 +0000

    datatypes were wrong for x86_64-linux system: on such systems long is 64bit, int is still 32bit
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@904 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/Common/MsxTypes.h
+++ blueMSX/Src/Common/MsxTypes.h
@@ -47,11 +47,11 @@ extern "C" {
  */
 typedef unsigned char    UInt8;
 typedef unsigned short   UInt16;
-typedef unsigned long    UInt32;
+typedef unsigned int     UInt32;
 typedef unsigned __int64 UInt64;
 typedef signed   char    Int8;
 typedef signed   short   Int16;
-typedef signed   long    Int32;
+typedef signed   int     Int32;
 
 // Define color stuff
 
