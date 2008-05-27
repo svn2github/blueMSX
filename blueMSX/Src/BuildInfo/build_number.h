969bdfb4f4ba57c8fc53a540147815efcab11747
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2008-05-27 02:50:36 +0000

    Updated version number
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1047 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 33552
+#define BUILD_NUMBER 33553
 
-#define BUILD_DATE "5/19 2008 1:56pm"
+#define BUILD_DATE "5/26 2008 7:46pm"
 
 #endif
--- blueMSX/Src/BuildInfo/version.h
+++ blueMSX/Src/BuildInfo/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define BLUE_MSX_VERSION_MAJOR 2
-#define BLUE_MSX_VERSION_MINOR 7
+#define BLUE_MSX_VERSION_MINOR 8
 
-#define BLUE_MSX_VERSION "2.7.2"
+#define BLUE_MSX_VERSION "2.8 beta"
 
 #endif
