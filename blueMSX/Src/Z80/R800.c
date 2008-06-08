94a4bcde07ad948ef210b1e8a64af96391e9959e
hap-hap <hap-hap@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2008-06-08 11:49:20 +0000

    copypaste + edit releasenotes from manual
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1061 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/Z80/R800.c
+++ blueMSX/Src/Z80/R800.c
@@ -1,9 +1,9 @@
 /*****************************************************************************
 ** $Source: /cygdrive/d/Private/_SVNROOT/bluemsx/blueMSX/Src/Z80/R800.c,v $
 **
-** $Revision: 1.37 $
+** $Revision: 1.38 $
 **
-** $Date: 2008-06-07 06:39:20 $
+** $Date: 2008-06-08 11:49:19 $
 **
 ** Author: Daniel Vik
 **
@@ -5736,7 +5736,7 @@ static void r800SwitchCpu(R800* r800) {
         r800->delay[DLY_POSTIO]    = freqAdjust * 3;
         r800->delay[DLY_M1]        = freqAdjust * ((r800->cpuFlags & CPU_ENABLE_M1) ? 2 : 0);
         r800->delay[DLY_XD]        = freqAdjust * 1;
-        r800->delay[DLY_IM]        = freqAdjust * 2;
+        r800->delay[DLY_IM]        = freqAdjust * 2; // should be 4, but currently will break vdp timing
         r800->delay[DLY_IM2]       = freqAdjust * 19;
         r800->delay[DLY_NMI]       = freqAdjust * 11;
         r800->delay[DLY_PARALLEL]  = freqAdjust * 2;
