14fe7ff684114e2d98ed2c71b724108261051d4b
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2009-04-19 19:50:38 +0000

    Updated version info
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1106 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 33686
+#define BUILD_NUMBER 33746
 
-#define BUILD_DATE "4/8 2009 8:28am"
+#define BUILD_DATE "4/19 2009 12:39pm"
 
 #endif
