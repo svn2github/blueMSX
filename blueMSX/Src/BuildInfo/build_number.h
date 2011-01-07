3f092d43280b8c91616153fc286dbda2cbd8c5c5
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2011-01-07 05:49:41 +0000

    Updated VC2010 makefile to make it build
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1189 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 34031
+#define BUILD_NUMBER 34032
 
-#define BUILD_DATE "12/30 2010 9:44pm"
+#define BUILD_DATE "1/6 2011 9:45pm"
 
 #endif
