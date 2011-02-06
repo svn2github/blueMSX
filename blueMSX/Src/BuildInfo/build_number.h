601d28f00b2d67f494f91627725559e2a5dfdfa8
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2011-02-06 08:08:42 +0000

    Added back 2003 compatible icon (only used for VC2003)
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1203 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 34033
+#define BUILD_NUMBER 34036
 
-#define BUILD_DATE "2/5 2011 9:42pm"
+#define BUILD_DATE "2/6 2011 12:7am"
 
 #endif
