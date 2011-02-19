62027996f16670bfc15b3a6deb9653d9f6a6637e
spectravision <spectravision@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2011-02-19 01:36:44 +0000

    Updated RS IDE ports
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1205 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/Memory/romMapperSvi328RsIDE.c
+++ blueMSX/Src/Memory/romMapperSvi328RsIDE.c
@@ -7,7 +7,7 @@
 **
 ** More info: http://www.bluemsx.com
 **
-** Copyright (C) 2003-2006 Daniel Vik
+** Copyright (C) 2003-2006 Daniel Vik, Tomas Karlsson
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -41,34 +41,34 @@
 #include <stdio.h>
 
 /*
-PPI    NAME   IDE PIN
----    ----   -------
-PA0    HD0    17 D0
-PA1    HD1    15 D1
-PA2    HD2    13 D2
-PA3    HD3    11 D3
-PA4    HD4     9 D4
-PA5    HD5     7 D5
-PA6    HD6     5 D6
-PA7    HD7     3 D7
-
-PB0    HD8     4 D8
-PB1    HD9     6 D9
-PB2    HD10    8 D10
-PB3    HD11   10 D11
-PB4    HD12   12 D12
-PB5    HD13   14 D13
-PB6    HD14   16 D14
-PB7    HD15   18 D15
-
-PC0    HA0    35 A0
-PC1    HA1    33 A1
-PC2    HA2    36 A2
-PC3    N/A
-PC4    N/A
-PC5    HCS    37 /CS0
-PC6    HWR    23 /IOWR
-PC7    HRD    25 /IORD
+PPI    NAME   IDE PIN     PPI    NAME   IDE PIN
+---    ----   -------     ---    ----   -------
+PA0    HD0    17 D0       PB0    HD0    17 D0  
+PA1    HD1    15 D1       PB1    HD1    15 D1  
+PA2    HD2    13 D2       PB2    HD2    13 D2  
+PA3    HD3    11 D3       PB3    HD3    11 D3  
+PA4    HD4     9 D4       PB4    HD4     9 D4  
+PA5    HD5     7 D5       PB5    HD5     7 D5  
+PA6    HD6     5 D6       PB6    HD6     5 D6  
+PA7    HD7     3 D7       PB7    HD7     3 D7
+                                               
+PB0    HD8     4 D8       PC0    HD8     4 D8  
+PB1    HD9     6 D9       PC1    HD9     6 D9  
+PB2    HD10    8 D10      PC2    HD10    8 D10 
+PB3    HD11   10 D11      PC3    HD11   10 D11 
+PB4    HD12   12 D12      PC4    HD12   12 D12 
+PB5    HD13   14 D13      PC5    HD13   14 D13 
+PB6    HD14   16 D14      PC6    HD14   16 D14 
+PB7    HD15   18 D15      PC7    HD15   18 D15 
+                                               
+PC0    HA0    35 A0       PA0    HA0    35 A0  
+PC1    HA1    33 A1       PA1    HA1    33 A1  
+PC2    HA2    36 A2       PA2    HA2    36 A2  
+PC3    N/A                PA3    HCS0   37 CS0 
+PC4    N/A                PA4    HCS1   38 CS1 
+PC5    HCS    37 /CS0     PA5    HWR    23 IOWR
+PC6    HWR    23 /IOWR    PA6    HRD    25 IORD
+PC7    HRD    25 /IORD    PA7    HRST   1   RST
 */
 
 typedef struct {
@@ -133,37 +133,26 @@ static UInt8 peekIo(RomMapperRsIde* rm, UInt16 ioPort)
     return 0xff;
 }
 
-static UInt8 readA(RomMapperRsIde* rm)
+static UInt8 readB(RomMapperRsIde* rm)
 {
     return (UInt8)rm->ideData;
 }
 
-static UInt8 readB(RomMapperRsIde* rm)
+static UInt8 readCLo(RomMapperRsIde* rm)
+{
+    return (UInt8)(rm->ideData >> 8);
+}
+
+static UInt8 readCHi(RomMapperRsIde* rm)
 {
     return (UInt8)(rm->ideData >> 8);
 }
 
 static void writeA(RomMapperRsIde* rm, UInt8 value)
 {
-    rm->ideData &= 0xff00;
-    rm->ideData |= value;
-}
-
-static void writeB(RomMapperRsIde* rm, UInt8 value)
-{
-    rm->ideData &= 0x00ff;
-    rm->ideData |= value<<8;
-}
-
-static void writeCLo(RomMapperRsIde* rm, UInt8 value)
-{
     rm->ideAddress = value & 0x07;
-}
-
-static void writeCHi(RomMapperRsIde* rm, UInt8 value)
-{
-    rm->ideIoRead = value & 0x08 ? 0:1;
-    rm->ideIoWrite = value & 0x04 ? 0:1;
+    rm->ideIoRead = value & 0x40 ? 0:1;
+    rm->ideIoWrite = value & 0x20 ? 0:1;
 
     if (rm->ideIoRead)
     {
@@ -190,6 +179,24 @@ static void writeCHi(RomMapperRsIde* rm, UInt8 value)
             break;
         }
     }
+}
+
+static void writeB(RomMapperRsIde* rm, UInt8 value)
+{
+    rm->ideData &= 0xff00;
+    rm->ideData |= value;
+}
+
+static void writeCLo(RomMapperRsIde* rm, UInt8 value)
+{
+    rm->ideData &= 0x00ff;
+    rm->ideData |= value<<8;
+}
+
+static void writeCHi(RomMapperRsIde* rm, UInt8 value)
+{
+    rm->ideData &= 0x00ff;
+    rm->ideData |= value<<8;
 }
 
 static void reset(RomMapperRsIde* rm)
@@ -221,12 +228,11 @@ int romMapperSvi328RsIdeCreate(int hdId)
     rm->deviceHandle = deviceManagerRegister(ROM_SVI328RSIDE, &callbacks, rm);
     rm->debugHandle = debugDeviceRegister(DBGTYPE_PORT, langDbgDevIdeSviRs(), &dbgCallbacks, rm);
 
-    rm->i8255 = i8255Create( NULL, readA, writeA,
-                             NULL, readB, writeB,
-                             NULL, NULL,  writeCLo,
-                             NULL, NULL,  writeCHi,
+    rm->i8255 = i8255Create( NULL, NULL,    writeA,
+                             NULL, readB,   writeB,
+                             NULL, readCLo, writeCLo,
+                             NULL, readCHi, writeCHi,
                              rm);
-
 
     ioPortRegister(0x14, i8255Read, i8255Write, rm->i8255); // PPI Port A
     ioPortRegister(0x15, i8255Read, i8255Write, rm->i8255); // PPI Port B
