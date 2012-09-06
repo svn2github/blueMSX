0116cdfa0de209c5db21aa5213afc0c0282bc7f1
spectravision <spectravision@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2012-09-06 14:36:05 +0000

    New mapper for the SVI-707 FDC
    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1227 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 34054
+#define BUILD_NUMBER 34055
 
-#define BUILD_DATE "8/30 2012 3:27pm"
+#define BUILD_DATE "8/30 2012 4:28pm"
 
 #endif
