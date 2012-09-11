d5da2e1f150a1674a3151dc9dc55219aff62e08a
spectravision <spectravision@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2012-09-11 08:48:54 +0000

    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1237 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
-#define BUILD_NUMBER 34055
+#define BUILD_NUMBER 34056
 
-#define BUILD_DATE "8/30 2012 4:28pm"
+#define BUILD_DATE "9/11 2012 10:46am"
 
 #endif
