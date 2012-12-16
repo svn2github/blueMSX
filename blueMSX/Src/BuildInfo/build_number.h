3b4f5ff303ab0d7bde48bfb34285481012fbe54c
dvik <dvik@c7fbdaac-a1cd-4d86-93b4-5ef691860ebf>
2012-12-16 05:24:18 +0000

    git-svn-id: svn://svn.code.sf.net/p/bluemsx/code/trunk@1263 c7fbdaac-a1cd-4d86-93b4-5ef691860ebf

--- blueMSX/Src/BuildInfo/build_number.h
+++ blueMSX/Src/BuildInfo/build_number.h
@@ -1,8 +1,16 @@
 #ifndef BUILD_NUMBER_H
 #define BUILD_NUMBER_H
 
+<<<<<<< .mine
+#define BUILD_NUMBER 34269
+=======
 #define BUILD_NUMBER 34058
+>>>>>>> .r1261
 
+<<<<<<< .mine
+#define BUILD_DATE "12/6 2012 12:27am"
+=======
 #define BUILD_DATE "10/22 2012 10:18pm"
+>>>>>>> .r1261
 
 #endif
